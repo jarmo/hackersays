dfaa9641c35484b9fb1d674f2399ebe555200965
Jarmo Pertman <jarmo.p@gmail.com>
2012-04-15 12:46:11 +0300

    add initial data as yaml

--- programming-quotes.rb
+++ programming-quotes.rb
@@ -1,6 +1,16 @@
+# encoding: UTF-8
 require "sinatra"
+require "haml"
+require "yaml"
+
+def quotes
+  #@quotes = YAML.load <<-EOF
+  #EOF
+  @quotes = YAML.load File.read(File.dirname(__FILE__) + "/data.yaml")
+end
 
 get '/' do
-  "Hello from Sinatra on Heroku!"
+  @quote = quotes.sample
+  haml :index
 end
 
