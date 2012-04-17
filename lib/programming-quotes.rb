75b8203619e187b6177da3e2f3b5c6f0a185aadb
Jarmo Pertman <jarmo.p@gmail.com>
2012-04-17 22:44:56 +0300

    use Sinatra::Base

--- lib/programming-quotes.rb
+++ lib/programming-quotes.rb
@@ -6,44 +6,48 @@ require "compass"
 require "yaml"
 require "yajl"
 
-configure do
-  Compass.configuration do |config|
-    config.project_path = File.dirname(__FILE__)
-    config.sass_dir = 'views'
-    config.output_style = :compressed
+class ProgrammingQuotes < Sinatra::Base
+  configure do
+    Compass.configuration do |config|
+      config.project_path = File.dirname(__FILE__)
+      config.sass_dir = 'views'
+      config.output_style = :compressed
+    end
+
+    set :haml, {:format => :html5}
+    set :scss, Compass.sass_engine_options
   end
 
-  set :haml, {:format => :html5}
-  set :scss, Compass.sass_engine_options
-end
+  def quotes
+    #@quotes = YAML.load <<-EOF
+    #EOF
+    f = File.open(File.dirname(__FILE__) + "/data.yaml", "r:utf-8")
+    @quotes = YAML.load f.read
+  ensure
+    f.close
+  end
 
-def quotes
-  #@quotes = YAML.load <<-EOF
-  #EOF
-  f = File.open(File.dirname(__FILE__) + "/data.yaml", "r:utf-8")
-  @quotes = YAML.load f.read
-ensure
-  f.close
-end
+  def format quote
+    quote[:c].gsub!($/, "<br>")
+    quote[:a] = quote[:a] || "Anonymous"
+    quote
+  end
 
-def format quote
-  quote[:c].gsub!($/, "<br>")
-  quote[:a] = quote[:a] || "Anonymous"
-  quote
-end
+  get '/' do
+    @quote = format quotes.sample
+    haml :index
+  end
 
-get '/' do
-  @quote = format quotes.sample
-  haml :index
-end
+  get '/quote' do
+    content_type 'application/json', :charset => 'utf-8'
+    Yajl::Encoder.encode format(quotes.sample)
+  end
 
-get '/quote' do
-  content_type 'application/json', :charset => 'utf-8'
-  Yajl::Encoder.encode format(quotes.sample)
-end
+  get '/style.css' do
+    content_type 'text/css', :charset => 'utf-8'
+    scss :style
+  end
 
-get '/style.css' do
-  content_type 'text/css', :charset => 'utf-8'
-  scss :style
-end
+  run! if app_file == $0
 
+end
