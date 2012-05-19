1d4662298f4406c75b303cb182543f8ea83628a6
Jarmo Pertman <jarmo.p@gmail.com>
2012-05-19 19:24:07 +0300

    using addthis for sharing.

--- public/script.js
+++ public/script.js
@@ -44,10 +44,6 @@ $(function() {
     History.replaceState(null, "Hacker Says - quote by " + slide.find("cite").text(), slide.data("id"));
     _gaq.push(['_trackPageview']);
 
-    renderTwitterButton();
-    renderFacebookButton();
-    renderGooglePlusButton();
-
     if (total - index == 5) loadNewQuotes();
     clearTimeout(readingTimer);
     readingTimer = setTimeout(function() {
@@ -83,35 +79,6 @@ $(function() {
     var time = (slide.find("span").text() + " " + slide.find("cite").text()).split(" ").length * 60 / averageWPM * additionalFactor;
     if (time < 5) time = 5;
     return time * 1000;
-  }
-
-  function renderTwitterButton() {
-    var btn = '<a href="https://twitter.com/share" class="twitter-share-button" data-text="I like this quote at programming-quotes.com" data-hashtags="programming" data-url="' + window.location.href + '">Tweet</a>';
-    $("#tweet-button").html(btn);
-
-    if (typeof twttr == 'undefined')
-      $.getScript("//platform.twitter.com/widgets.js", load)
-    else
-      load()
-
-    function load() { twttr.widgets.load() }
-  }
-
-  function renderFacebookButton() {
-    var btn = '<div class="fb-like" data-send="false" data-layout="button_count" data-width="70" data-show-faces="false" data-font="arial"></div>';
-    $("#fb-button").html(btn);
-    if (typeof FB == 'undefined')
-      $.getScript("//connect.facebook.net/en_US/all.js#xfbml=1", parse)
-    else
-      parse()
-
-    function parse() { FB.XFBML.parse() }
-  }
-
-  function renderGooglePlusButton() {
-    var btn = '<div class="g-plusone" data-size="small" data-href="' + window.location.href + '"></div>';
-    $("#gplus-button").html(btn);
-    gapi.plusone.go();
   }
 
 });
